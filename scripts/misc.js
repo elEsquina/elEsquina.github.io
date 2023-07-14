2c446d757b56d3d7558fa11ee5614fedc50c95e4
elEsquina <o.azoubi@hotmail.com>
2023-07-15 00:42:31 +0100

    yes

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,24 +1,35 @@
-function getRandomDelay(){
-    return Math.floor( Math.random() * 10000 ) + 1000;
+let cloudInterval;
+const cloudContainer = document.getElementById("cloud_container");
+
+function getRandomDelay() {
+  return Math.floor(Math.random() * 5000) + 1000;
 }
 
-function generateClouds(){
-    const cloudContainer = document.getElementById("cloud_container");
 
-    setInterval(() => {
-        
-        if (cloudContainer.childElementCount > 10) {
-            return;
-        }
+function generateClouds() {
+  cloudInterval = setInterval(() => {
+    if (cloudContainer.childElementCount >= 15) {
+      return;
+    }
 
-        const cloud = document.createElement("div");
-        cloud.classList.add("cloud" + Math.floor(Math.random() * 2 + 1) );
-        cloud.style.top = Math.floor(Math.random() * 100) + "%";
+    const cloud = document.createElement("div");
+    cloud.classList.add("cloud" + Math.floor(Math.random() * 2 + 1));
+    cloud.style.top = Math.floor(Math.random() * 100) + "%";
 
-        cloudContainer.appendChild(cloud);
+    cloudContainer.appendChild(cloud);
 
-    }, getRandomDelay());
-
+  }, getRandomDelay());
 }
 
-generateClouds();
+
+function stopClouds() {
+  clearInterval(cloudInterval);
+  while (cloudContainer.firstChild) {
+    cloudContainer.firstChild.remove();
+  }
+}
+
+
+window.addEventListener('beforeunload', function() {stopClouds(); });
+window.addEventListener("pageshow", generateClouds);
+
