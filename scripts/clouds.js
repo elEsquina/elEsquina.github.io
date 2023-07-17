e240b05102cca47188b790d816f2a78829a73bb1
elEsquina <o.azoubi@hotmail.com>
2023-07-17 22:33:39 +0100

    she is great

--- scripts/clouds.js
+++ scripts/clouds.js
@@ -1,41 +1,33 @@
-let cloudInterval;
 const cloudContainer = document.getElementById("cloud_container");
 let cloudsGenerated = false;
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function getRandomDelay() {
   return Math.floor(Math.random() * 5000) + 1000;
 }
 
-function generateClouds() {
+
+async function generateClouds() {
+
   if (cloudsGenerated) {
     return;
   }
   cloudsGenerated = true;
 
-  cloudInterval = setInterval(() => {
-    if (cloudContainer.childElementCount >= 15) {
-      return;
-    }
+  while (!(cloudContainer.childElementCount >= 15)){
 
     const cloud = document.createElement("div");
     cloud.classList.add("cloud" + Math.floor(Math.random() * 2 + 1));
     cloud.style.top = Math.floor(Math.random() * 100) + "%";
 
     cloudContainer.appendChild(cloud);
-  }, getRandomDelay());
 
-}
-
-function stopClouds() {
-  clearInterval(cloudInterval);
-  while (cloudContainer.firstChild) {
-    cloudContainer.firstChild.remove();
+    await wait(getRandomDelay());
   }
+
 }
 
-window.addEventListener('beforeunload', function() {
-  stopClouds();
-  cloudsGenerated = false;
-});
-
-window.addEventListener("pageshow", generateClouds);
+generateClouds();
--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,35 @@ let currentpage = 0;
 let currentindx = "main";
 let running = false;
 
+
+let projects_page = 0;
+let projectsjson;
+
 const menus = {
   main: document.getElementById("real_container"),
-  aboutme: document.getElementById("about-me"), 
+  aboutme: document.getElementById("about-me"),
+  projects: document.getElementById("container--projects") 
 }
 
 
-function main(){
+async function main(){
+
+  projectsjson = await fetch("portfolio.json");
+  projectsjson = await projectsjson.json();
+  load_project_page();
 
   const button_container = document.getElementById('real_container');
 
   for (let i = 0; i < button_container.children.length; i++) {
     const child = button_container.children[i];
+    console.log(i);
 
-    child.addEventListener('click', () => {
-      currentpage = i + 1;
-      onpagechange();
-    })
+    if (i!=0)
+      child.addEventListener('click', () => {
+        currentpage = i;
+        onpagechange();
+      })
+
   }
 }
 
@@ -34,6 +46,18 @@ function goToMenu(){
 }
 
 
+function left_project(){
+  projects_page = Math.abs((projects_page - 1) % projectsjson.length);
+  load_project_page();
+}
+
+
+function right_project(){
+  projects_page = Math.abs((projects_page + 1) % projectsjson.length);
+  load_project_page();
+}
+
+
 function onpagechange(){
 
   if (running)
@@ -45,17 +69,19 @@ function onpagechange(){
   switch (currentpage) {
     case 0:
       toggle_element(menus.main, true);
-      document.querySelector('h1').textContent = "Hello!";
       currentindx = "main";
       break;
     
     case 1:
       toggle_element(menus.aboutme, true);
-      document.querySelector('h1').textContent = "About me!";
       currentindx = "aboutme";
       break;
     
     case 2:
+      toggle_element(menus.projects, true);
+      currentindx = "projects";
+      break;
+    
   }
   
 }
@@ -77,5 +103,12 @@ async function toggle_element(element, on){
   running = false;
 }
 
+
+function load_project_page(){
+  document.getElementById("project_title").innerHTML =  projectsjson[projects_page].title;
+  document.getElementById("project_desc").innerHTML =  projectsjson[projects_page].desc;
+  document.getElementById("project_video").src =  projectsjson[projects_page].url;
+  document.getElementById("project_page_count").innerHTML =  "Page " + (projects_page + 1) + "/" + projectsjson.length;
+}
 
 main();
